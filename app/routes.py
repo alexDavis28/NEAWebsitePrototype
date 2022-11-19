c8037a128a59f8defed41b8aa91ec8666e80d9ab
Alex Davis <8m3xlt@gmail.com>
2022-11-19 13:11:15 +0000

    Basic Form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from app.forms import RecommenderForm
+from flask import render_template
 
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    form = RecommenderForm()
+    return render_template("index.html", form=form)
